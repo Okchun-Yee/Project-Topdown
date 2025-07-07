5a02e841056fdd3ff9c14cb6348c72184b9088bc
한채훈 <han020813@gmail.com>
2025-07-07 11:59:41 +0900

    feat: Destructible 오브젝트 파괴시 드랍되는 아이템에 역동성 구현
    BUG 내용: Arrow 및 전체적인 Projectile 에 충돌이 발생하지 않고 있음.
    다음 작업 목표: Projectile 버그 수정
    코멘트: .

--- Assets/Script/Pickups.cs
+++ Assets/Script/Pickups.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Pickups : MonoBehaviour
 {
     [SerializeField] private float pickUpDistance = 5f;
     [SerializeField] private float accelartionRate = .2f;
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
     private Vector3 moveDir;
     private Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        // 초기 위치에서 위로 튕겨오르는 애니메이션
+        StartCoroutine(AnimCurveSpawnRoutine());
     }
     private void Update()
     {
@@ -34,6 +43,26 @@ public class Pickups : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.GetComponent<PlayerController>()){ Destroy(gameObject); }
+        if (collision.gameObject.GetComponent<PlayerController>()) { Destroy(gameObject); }
+    }
+    private IEnumerator AnimCurveSpawnRoutine()
+    {
+        Vector2 startPos = transform.position;
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+
+        Vector2 endPoint = new Vector2(randomX, randomY);
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linerT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linerT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPos, endPoint, linerT) + new Vector2(0f, height);
+            yield return null;
+        }
     }
 }
