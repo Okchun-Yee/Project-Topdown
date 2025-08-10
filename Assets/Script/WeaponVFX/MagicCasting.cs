4e2ec676ab9127d97d10db6eeb0e2f0cb90032b3
한채훈 <han020813@gmail.com>
2025-08-10 22:00:19 +0900

    working: 캐스팅 로직 작업 중

--- Assets/Script/WeaponVFX/MagicCasting.cs
+++ Assets/Script/WeaponVFX/MagicCasting.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicCasting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isCasted = false;  
+    private float castingTimer = 0f;
+    private void Update()
     {
-        
+        if (isCasted)
+        {
+            // Implement your skill casting logic here
+            castingTimer -= Time.deltaTime;
+            if (castingTimer <= 0f)
+            {
+                Debug.Log("Skill ON!");
+                isCasted = false; // 시전 완료 후 상태 초기화
+                // 실제 스킬 발동 코드
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SkillCasting(float castingTime)
     {
-        
+        if (isCasted) { return; } // 이미 시전 중이면 무시
+        castingTimer = castingTime;
+        isCasted = true;
+        Debug.Log("Skill Casting Start!");
     }
 }
